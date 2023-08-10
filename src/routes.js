c8d1f48f88b764c3c782a061eec609dedc199555
Shivam Taneja <76044999+ramen-7@users.noreply.github.com>
2023-08-10 19:54:25 +0530

    commits

--- src/routes.js
+++ src/routes.js
@@ -18,6 +18,7 @@ import RTL from "views/admin/rtl";
 
 // Auth Imports
 import SignInCentered from "views/auth/signIn";
+import SignUpCentered from "views/auth/signUp";
 
 const routes = [
   {
@@ -63,6 +64,13 @@ const routes = [
     icon: <Icon as={MdLock} width='20px' height='20px' color='inherit' />,
     component: SignInCentered,
   },
+  {
+    name: "Sign Up",
+    layout: "/auth",
+    path: "/sign-up",
+    icon: <Icon as={MdLock} width='20px' height='20px' color='inherit' />,
+    component: SignUpCentered,
+  }
   // {
   //   name: "RTL Admin",
   //   layout: "/rtl",
