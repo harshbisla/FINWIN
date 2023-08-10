eeb2645559d1a1e0495d664bfb2583c897b51654
Shivam Taneja <76044999+ramen-7@users.noreply.github.com>
2023-08-10 20:20:31 +0530

    signup and sign in page changes

--- src/views/auth/signIn/index.jsx
+++ src/views/auth/signIn/index.jsx
@@ -104,27 +104,24 @@ function SignIn() {
           mx={{ base: "auto", lg: "unset" }}
           me='auto'
           mb={{ base: "20px", md: "auto" }}>
-          <Button
-            fontSize='sm'
-            me='0px'
-            mb='26px'
-            py='15px'
-            h='50px'
-            borderRadius='16px'
-            bg={googleBg}
-            color={googleText}
-            fontWeight='500'
-            _hover={googleHover}
-            _active={googleActive}
-            _focus={googleActive}>
-            <Icon as={FcGoogle} w='20px' h='20px' me='10px' />
-            Sign in with Google
-          </Button>
+          {/*<Button*/}
+          {/*  fontSize='sm'*/}
+          {/*  me='0px'*/}
+          {/*  mb='26px'*/}
+          {/*  py='15px'*/}
+          {/*  h='50px'*/}
+          {/*  borderRadius='16px'*/}
+          {/*  bg={googleBg}*/}
+          {/*  color={googleText}*/}
+          {/*  fontWeight='500'*/}
+          {/*  _hover={googleHover}*/}
+          {/*  _active={googleActive}*/}
+          {/*  _focus={googleActive}>*/}
+          {/*  <Icon as={FcGoogle} w='20px' h='20px' me='10px' />*/}
+          {/*  Sign in with Google*/}
+          {/*</Button>*/}
           <Flex align='center' mb='25px'>
             <HSeparator />
-            <Text color='gray.400' mx='14px'>
-              or
-            </Text>
             <HSeparator />
           </Flex>
           <FormControl>
@@ -177,19 +174,19 @@ function SignIn() {
             </InputGroup>
             <Flex justifyContent='space-between' align='center' mb='24px'>
               <FormControl display='flex' alignItems='center'>
-                <Checkbox
-                  id='remember-login'
-                  colorScheme='brandScheme'
-                  me='10px'
-                />
-                <FormLabel
-                  htmlFor='remember-login'
-                  mb='0'
-                  fontWeight='normal'
-                  color={textColor}
-                  fontSize='sm'>
-                  Keep me logged in
-                </FormLabel>
+                {/*<Checkbox*/}
+                {/*  id='remember-login'*/}
+                {/*  colorScheme='brandScheme'*/}
+                {/*  me='10px'*/}
+                {/*/>*/}
+                {/*<FormLabel*/}
+                {/*  htmlFor='remember-login'*/}
+                {/*  mb='0'*/}
+                {/*  fontWeight='normal'*/}
+                {/*  color={textColor}*/}
+                {/*  fontSize='sm'>*/}
+                {/*  Keep me logged in*/}
+                {/*</FormLabel>*/}
               </FormControl>
               <NavLink to='/auth/forgot-password'>
                 <Text
